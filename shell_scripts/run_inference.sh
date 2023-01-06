45cc4ea90719ad29b571ec490caaffcf6b782461
Kim-hanjun <87162907+Kim-hanjun@users.noreply.github.com>
2023-01-06 17:31:26 +0900

    Update run_inference.sh
    add merge_tsv_path

--- shell_scripts/run_inference.sh
+++ shell_scripts/run_inference.sh
@@ -1,6 +1,8 @@
 MODEL_PATH="output/klue/bert-base"
 TEST_TSV_PATH="data/preprocess/test.tsv"
+MERGE_TSV_PATH="data/raw/merge.tsv"
 
 python -m inference \
   --model_path ${MODEL_PATH} \
-  --test_tsv_path ${TEST_TSV_PATH}
+  --test_tsv_path ${TEST_TSV_PATH} \
+  --merge_tsv_path ${MERGE_TSV_PATH}
